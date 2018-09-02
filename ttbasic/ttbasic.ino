095b3b84aa025882438f02fa33b2eada1e7d0ebd
たま吉さん(Tamakichi-San) <tamacat2014@gmail.com>
2018-09-02 20:47:52 +0900

    I2CCLKコマンド追加、TFT版条件付きコンパイルミス修正

--- ttbasic/ttbasic.ino
+++ ttbasic/ttbasic.ino
@@ -23,7 +23,11 @@ void setup(void){
   nvic_irq_set_priority(NVIC_TIMER2,2);
 
 #if defined (__STM32F1__)   
+#ifdef STM32_R20170323
   for(uint8_t tm=0; tm <15 && !Serial.isConnected(); tm++) {
+#else
+  for(uint8_t tm=0; tm <15 && !Serial; tm++) {
+#endif
     delay(100);
   }
 #endif
